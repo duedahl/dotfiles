fbd2fde73a7fa8fdb3b6e0121912c0bb44056c61
Mathias Duedahl <mail@duedahl.dev>
2025-05-20 22:59:36 +0200

    Added gef config

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,15 +1,16 @@
 local wezterm = require 'wezterm'
 local config = wezterm.config_builder()
 
+-- Functionality Changes
+config.enable_tab_bar = false
+
 -- General appearance
-config.color_scheme = 'Catppuccin Mocha'  -- A popular dark theme
+config.color_scheme = 'Catpuccin Mocha'
 config.font = wezterm.font('JetBrains Mono')
 config.font_size = 10.0
 config.window_background_opacity = 0.8
 
 -- GNOME/X11 specific configurations
 config.enable_wayland = false
-config.enable_tab_bar = true
-config.use_fancy_tab_bar = true
 
 return config
